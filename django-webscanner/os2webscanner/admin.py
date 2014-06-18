9dbb806ff3ac9109b297a9bd345af7c0a1fcf3e9
Carsten Agger <carstena@magenta.dk>
2014-06-18 18:04:51 +0200

    Integrate user profile in user admin interface.

--- django-webscanner/os2webscanner/admin.py
+++ django-webscanner/os2webscanner/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
 
@@ -6,6 +8,17 @@ from .models import Organization, UserProfile, Domain, RegexRule, Scanner
 from .models import Scan, Match, Url, ConversionQueueItem
 
 ar = admin.site.register
-classes = [Organization, UserProfile, Domain, RegexRule, Scanner, Scan, Match,
+classes = [Organization, Domain, RegexRule, Scanner, Scan, Match,
            Url, ConversionQueueItem]
 map(ar, classes)
+
+class ProfileInline(admin.TabularInline):
+    model = UserProfile
+    extra = 1
+
+class MyUserAdmin(UserAdmin):
+    inlines = [ProfileInline]
+    can_delete = False
+
+admin.site.unregister(User)
+admin.site.register(User, MyUserAdmin)
