70f364eff84053a20a38d2792648676b22429919
Seth Yastrov <seth@magenta.dk>
2014-06-26 09:31:05 +0200

    Processing HTML: Replace entities and remove tags except comments. Process text both before and after removing tags.

--- scrapy-webscanner/scanner/processors/html.py
+++ scrapy-webscanner/scanner/processors/html.py
@@ -1,8 +1,16 @@
 from processor import Processor
+from w3lib.html import remove_tags, replace_entities
 
 class HTMLProcessor(Processor):
     def process(self, response):
         print "Process HTML", response.url
-        # TODO: Strip HTML tags, convert HTML entities to text
-        # TODO: Pass to Text processor
-        return response.body.decode(response.encoding)
+        html = response.body.decode(response.encoding)
+        # Convert HTML entities to their unicode representation
+        entity_replaced_html = replace_entities(html)
+        # Strip tags from the HTML (except comments)
+        no_tags_html = remove_tags(entity_replaced_html, keep=("!--"))
+        # Match against HTML before AND after removing tags
+        # Because it could be possible that a tag interferes with a regex
+        # matching (And it could also be that it does not)
+        match_against = entity_replaced_html + " " + no_tags_html
+        return match_against
