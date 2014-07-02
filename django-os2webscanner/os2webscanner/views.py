28d7c18d7f358b2fe3e03169ca70a6ae2ff7dea6
Carsten Agger <carstena@magenta.dk>
2014-07-02 14:55:04 +0200

    Restrict users to stuff from their own organization.

--- django-os2webscanner/os2webscanner/views.py
+++ django-os2webscanner/os2webscanner/views.py
@@ -3,7 +3,7 @@ from django.views.generic import View, ListView, TemplateView
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 
-from .models import Scanner, Domain, RegexRule, Scan
+from .models import Scanner, Domain, RegexRule, Scan, UserProfile
 
 
 class LoginRequiredMixin(View):
@@ -14,23 +14,40 @@ class LoginRequiredMixin(View):
         return super(LoginRequiredMixin, self).dispatch(*args, **kwargs)
 
 
+class RestrictedListView(ListView, LoginRequiredMixin):
+    """Make sure users only see data that belong to their own organization."""
+    
+    def get_queryset(self):
+        """Restrict to the organization of the logged-in user."""
+        user = self.request.user
+        try:
+            profile = user.get_profile()
+        except UserProfile.DoesNotExist:
+            profile = None
+
+        if profile:
+            return self.model.objects.filter(organization=profile.organization)
+        else:
+            return self.model.objects.all()
+
+
 class MainPageView(TemplateView):
     template_name = 'index.html'
 
 
-class ScannerList(ListView, LoginRequiredMixin):
+class ScannerList(RestrictedListView):
     """Displays list of scanners."""
     model = Scanner
     template_name = 'os2webscanner/scanners.html'
 
 
-class DomainList(ListView, LoginRequiredMixin):
+class DomainList(RestrictedListView):
     """Displays list of domains."""
     model = Domain
     template_name = 'os2webscanner/domains.html'
 
 
-class RuleList(ListView, LoginRequiredMixin):
+class RuleList(RestrictedListView):
     """Displays list of scanners."""
     model = RegexRule
     template_name = 'os2webscanner/rules.html'
