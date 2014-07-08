a255db9bf43ecf275febb268e02a44f6c8746ec5
Carsten Agger <carstena@magenta.dk>
2014-07-08 09:33:07 +0200

    Email notification in place.

--- django-os2webscanner/os2webscanner/utils.py
+++ django-os2webscanner/os2webscanner/utils.py
@@ -2,6 +2,8 @@ from django.conf import settings
 from django.core.mail import EmailMessage
 from django.template import loader, Context
 
+import models 
+
 
 def notify_user(scan):
     """Notify user about completed scan - including success and failure."""
@@ -9,18 +11,25 @@ def notify_user(scan):
 
     t = loader.get_template(template)
 
-    c = Context({'scan': scan, 'domain': settings.SITE_URL})
-
     subject = "Scanning afsluttet: {0}".format(scan.status_text)
     to_address = scan.scanner.organization.contact_email
     if not to_address:
         to_address = settings.ADMIN_EMAIL
+    matches = models.Match.objects.filter(scan=scan).count()
+    critical = models.Match.objects.filter(
+        scan=scan,
+        sensitivity=models.Sensitivity.HIGH
+    ).count()
+
+    c = Context({'scan': scan, 'domain': settings.SITE_URL, 'matches': matches,
+                 'critical': critical})
 
     try:
         body = t.render(c)
         message = EmailMessage(subject, body, settings.ADMIN_EMAIL,
-                               [to_address])
+                               [to_address,])
         message.send()
+        print "Mail sendt til", to_address
     except Exception as e:
         # TODO: Handle this properly
         raise
--- webscanner_site/webscanner/settings.py
+++ webscanner_site/webscanner/settings.py
@@ -71,7 +71,7 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'da-dk'
 
 TIME_ZONE = 'UTC'
 
