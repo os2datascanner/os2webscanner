292018d5c89d6715cbb77b1a449c8093a00dbb4a
Carsten Agger <agger@webscanner.magenta-aps.dk>
2014-07-09 17:27:02 +0200

    First version of CSV writer.

--- django-os2webscanner/os2webscanner/utils.py
+++ django-os2webscanner/os2webscanner/utils.py
@@ -1,3 +1,6 @@
+import csv
+import StringIO
+
 from django.conf import settings
 from django.core.mail import EmailMessage
 from django.template import loader, Context
@@ -33,3 +36,16 @@ def notify_user(scan):
     except Exception as e:
         # TODO: Handle this properly
         raise
+
+
+def write_csv_report(scan):
+    """Create a CSV file with report data. Return as an iterable."""
+    all_matches = models.Match.objects.filter(scan=scan).order_by(
+        '-sensitivity', 'url', 'matched_rule', 'matched_data'
+    )
+    output = StringIO.StringIO()
+    report_writer = csv.writer(output)
+    for match in all_matches:
+        report_writer.writerow([match.url, match.matched_rule,
+                                match.matched_data, match.sensitivity])
+    print report_writer.get_value()
