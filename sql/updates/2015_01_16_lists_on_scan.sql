318f1f47079f5989d7223065a4908580fb32c918
Carsten Agger <carstena@magenta.dk>
2015-01-15 13:15:48 +0100

    Remove old field.

--- sql/updates/2015_01_16_lists_on_scan.sql
+++ sql/updates/2015_01_16_lists_on_scan.sql
@@ -1,3 +1,5 @@
 alter table os2webscanner_scan add    "blacklisted_names" text NOT NULL default '';
 alter table os2webscanner_scan add    "whitelisted_addresses" text NOT NULL default '';
 alter table os2webscanner_scan add    "blacklisted_addresses" text NOT NULL default '';
+
+alter table os2webscanner_scanner drop "whitelisted_names";
