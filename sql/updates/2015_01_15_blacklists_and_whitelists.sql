987c4f10ff917f6e0c8e3e966e63e6705b48fe3f
Carsten Agger <carstena@magenta.dk>
2015-01-16 15:48:53 +0100

    Fix SQL bug.

--- sql/updates/2015_01_15_blacklists_and_whitelists.sql
+++ sql/updates/2015_01_15_blacklists_and_whitelists.sql
@@ -1,5 +1,4 @@
-alter table os2webscanner_organization add 
-    "name_whitelist" text NOT NULL default '',
-    "name_blacklist" text NOT NULL default '',
-    "address_whitelist" text NOT NULL default '',
-    "address_blacklist" text NOT NULL default '';
+alter table os2webscanner_organization add "name_whitelist" text NOT NULL default '';
+alter table os2webscanner_organization add "name_blacklist" text NOT NULL default '';
+alter table os2webscanner_organization add "address_whitelist" text NOT NULL default '';
+alter table os2webscanner_organization add "address_blacklist" text NOT NULL default '';
