c899f92dfce4d5a3a4650471bdbe356e8265e3eb
Carsten Agger <carstena@magenta.dk>
2015-03-13 08:22:04 +0100

    Fix label, add help text.

--- django-os2webscanner/os2webscanner/forms.py
+++ django-os2webscanner/os2webscanner/forms.py
@@ -66,7 +66,8 @@ class FileUploadForm(forms.Form):
                                                initial="ADRESSE",
                                               )
     # Column list - ignored if left blank
-    column_list = forms.RegexField(label="Scan kolonner",
+    column_list = forms.RegexField(label="Scan kun kolonner",
+                                   help_text='Angiv kolonner som store bogstaver adskilt med komma',
                                     regex='^[A-Z,]+$',
                                    required=False,
                                    max_length=128)
