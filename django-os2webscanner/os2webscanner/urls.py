04d08ab7d6faf58715e30e353a578e82bb76a72c
Carsten Agger <carstena@magenta.dk>
2017-03-20 14:30:07 +0100

    Reset password flow in place.

--- django-os2webscanner/os2webscanner/urls.py
+++ django-os2webscanner/os2webscanner/urls.py
@@ -104,6 +104,27 @@ urlpatterns = patterns(
         {'template_name': 'password_change_done.html'},
         name='password_change_done'
         ),
+    url(r'^accounts/password_reset/$',
+        'django.contrib.auth.views.password_reset',
+        {'template_name': 'password_reset_form.html'},
+        name='password_reset'
+        ),
+    url(r'^accounts/password_reset/done/',
+        'django.contrib.auth.views.password_reset_done',
+        {'template_name': 'password_reset_done.html'},
+        name='password_reset_done'
+        ),
+    url(r'^accounts/reset/(?P<uidb64>[0-9A-Za-z_\-]+)/' +
+        '(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/',
+        'django.contrib.auth.views.password_reset_confirm',
+        {'template_name': 'password_reset_confirm.html'},
+        name='password_reset_confirm'
+        ),
+    url(r'^accounts/reset/complete',
+        'django.contrib.auth.views.password_reset_complete',
+        {'template_name': 'password_reset_complete.html'},
+        name='password_reset_complete'
+        ),
 
     # General dialog success handler
     url(r'^(scanners|domains|rules|groups|summaries)/(\d+)/(created)/$',
