38220ff7532df06e1f8b297520a306948a53140d
Danni Als <danni@magenta.dk>
2018-05-07 09:28:24 +0000

    * Old model files deleted

--- django-os2webscanner/os2webscanner/models/webconversionqueueitem_model.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-from .conversionqueueitem_model import ConversionQueueItem
-from .weburl_model import WebUrl
-
-
-class WebConversionQueueItem(ConversionQueueItem):
-    url = models.ForeignKey(WebUrl, null=False, verbose_name='Url')
