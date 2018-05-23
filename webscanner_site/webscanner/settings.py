22c3980a3ffb45e932a346f353f24af38926efe5
danni als <danni@magenta.dk>
2018-05-23 14:19:02 +0200

    is now using relative path for static files.

--- webscanner_site/webscanner/settings.py
+++ webscanner_site/webscanner/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/home/os2/os2webscanner/webscanner_site/static'
+STATIC_ROOT = BASE_DIR + 'webscanner_site/static'
 
 AUTH_PROFILE_MODULE = 'os2webscanner.UserProfile'
 
