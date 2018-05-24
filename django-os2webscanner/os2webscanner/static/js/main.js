896cdeb9372a1fe683e2f237c5d3c7d0f2fc87bf
Toke Fritzemeier <toke@magenta-aps.dk>
2018-05-24 16:55:09 +0200

    Made input text field so that we can filter long list of rules against a string; made the dropdown a max height

--- django-os2webscanner/os2webscanner/static/js/main.js
+++ django-os2webscanner/os2webscanner/static/js/main.js
@@ -49,3 +49,23 @@ function handleSubChoices(choice) {
     siblings.prop("checked", false);
   }
 }
+
+/* debounce function from https://davidwalsh.name/javascript-debounce-function */
+// Returns a function, that, as long as it continues to be invoked, will not
+// be triggered. The function will be called after it stops being called for
+// N milliseconds. If `immediate` is passed, trigger the function on the
+// leading edge, instead of the trailing.
+function os2debounce(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
