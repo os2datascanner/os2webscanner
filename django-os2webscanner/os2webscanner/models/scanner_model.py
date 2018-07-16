4b53285ef9853c61cecca6d7fcfa993e71212b50
dals83 <danni@magenta.dk>
2018-07-16 13:43:36 +0200

    * Removed unused import.

--- django-os2webscanner/os2webscanner/models/scanner_model.py
+++ django-os2webscanner/os2webscanner/models/scanner_model.py
@@ -33,7 +33,6 @@ from .organization_model import Organization
 from .group_model import Group
 from .regexrule_model import RegexRule
 from .userprofile_model import UserProfile
-from .exchangescan_model import ExchangeScan
 
 base_dir = os.path.dirname(
     os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
