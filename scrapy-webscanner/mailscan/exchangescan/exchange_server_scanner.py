931c5aa654fe181779e110b213409543a84168e1
dals83 <danni@magenta.dk>
2018-07-24 09:45:16 +0200

    * self.scan_id did not have a value yet. It does now.

--- scrapy-webscanner/mailscan/exchangescan/exchange_server_scanner.py
+++ scrapy-webscanner/mailscan/exchangescan/exchange_server_scanner.py
@@ -11,16 +11,16 @@ class ExchangeServerScanner(multiprocessing.Process):
     run a number of exporters in parallel """
     def __init__(self, user_queue, domain, scan_id, start_date=None):
         multiprocessing.Process.__init__(self)
-        from scanner.scanner.scanner import Scanner
-        self.scanner = Scanner(self.scan_id)
-        self.logger = init_logger(self.__class__.__name__,
-                                  self.scanner,
-                                  logging.DEBUG)
         self.user_queue = user_queue
         self.user_name = None
         self.start_date = start_date
         self.domain = domain
         self.scan_id = scan_id
+        from scanner.scanner.scanner import Scanner
+        self.scanner = Scanner(self.scan_id)
+        self.logger = init_logger(self.__class__.__name__,
+                                  self.scanner,
+                                  logging.DEBUG)
 
     def run(self):
         while not self.user_queue.empty():
