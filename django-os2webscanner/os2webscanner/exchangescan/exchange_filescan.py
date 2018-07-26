bb0eb61247ebd0c155582faaa11d53bed7eb964a
dals83 <danni@magenta.dk>
2018-07-26 14:52:54 +0200

    * filescan is now started from the right directory (scanner_dir). * folder to scan path is now stored temporary in db.

--- django-os2webscanner/os2webscanner/exchangescan/exchange_filescan.py
+++ django-os2webscanner/os2webscanner/exchangescan/exchange_filescan.py
@@ -10,6 +10,8 @@ import queue
 import multiprocessing
 import subprocess
 
+from django.conf import settings
+
 from .mailscan_exchange import ExchangeServerScan, read_users
 from .settings import NUMBER_OF_EMAIL_THREADS
 from os2webscanner.models.domain_model import Domain
@@ -20,14 +22,19 @@ class ExchangeFilescanner(object):
     def __init__(self, scan_id):
         print('Program started')
         self.scan_id = scan_id
-        from os2webscanner.models.scan_model import Scan
-        self.scan_object = Scan.objects.get(pk=scan_id)
 
     def start_mail_scan(self):
-        valid_domains = self.scan_object.domains.filter(
+        from os2webscanner.models.scan_model import Scan
+        scan_object = Scan.objects.get(pk=self.scan_id)
+        valid_domains = scan_object.domains.filter(
             validation_status=Domain.VALID
         )
 
+        if not os.path.exists(scan_object.scan_dir):
+            os.makedirs(scan_object.scan_dir)
+
+        scan_dir = scan_object.scan_dir
+        
         for domain in valid_domains:
             credentials = (domain.authentication.username,
                            domain.authentication.get_password())
@@ -35,7 +42,6 @@ class ExchangeFilescanner(object):
             read_users(user_queue,
                        domain.exchangedomain.get_userlist_file_path())
             done_queue = multiprocessing.Queue()
-            scan_dir = self.scan_object.scan_dir
             mail_ending = domain.url
 
             scanners = {}
@@ -59,6 +65,8 @@ class ExchangeFilescanner(object):
                 self.get_queue_item(done_queue)
                 time.sleep(1)
 
+        # TODO: Clean up scan_dir
+
     def get_queue_item(self, q):
         """
         Getting next queue item and starting file scan on item, until queue is empty.
@@ -80,12 +88,15 @@ class ExchangeFilescanner(object):
         Starts a file scan on downloaded exchange folder
         :param path: path to folder
         """
-        self.scan_object.exchangescan.folder_to_scan = path
-        scanner__dir = self.scan_object.scan_dir
-        log_file = open(self.scan_object.scan_log_file, "a")
+        from os2webscanner.models.scan_model import Scan
+        scan_object = Scan.objects.get(pk=self.scan_id)
+        scan_object.exchangescan.folder_to_scan = path
+        scan_object.save()
+        scanner_dir = os.path.join(settings.PROJECT_DIR, "scrapy-webscanner")
+        log_file = open(scan_object.scan_log_file, "a")
         try:
-            process = subprocess.Popen([os.path.join(scanner__dir, 'run.sh'),
-                                        str(self.scan_id)], cwd=scanner__dir,
+            process = subprocess.Popen([os.path.join(scanner_dir, 'run.sh'),
+                                        str(self.scan_id)], cwd=scanner_dir,
                                        stderr=log_file,
                                        stdout=log_file)
             process.communicate()
