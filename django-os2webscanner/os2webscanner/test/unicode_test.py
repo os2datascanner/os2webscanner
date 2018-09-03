35536802ed7a910525b000da693c0f9b065c8828
jgj-magenta-aps <jgj@magenta-aps.dk>
2018-09-03 11:42:46 +0200

    decoding file names

---
+++ django-os2webscanner/os2webscanner/test/unicode_test.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.models import AnonymousUser, User
+from django.test import TestCase, RequestFactory
+from os2webscanner.utils import get_codec_and_string
+
+
+class UnicodeTest(TestCase):
+    """Unit-tests for Unicode Dammit"""
+
+
+    def test_get_codec_and_string(self):
+        mybytes = "ÆØÅ#¤%&".encode("utf-16")
+        encoding, mystring = get_codec_and_string(mybytes)
+        self.assertEqual(get_codec_and_string(mybytes), ("UTF-16", "ÆØÅ#¤%&"))
+
