223cd889a230c6bdd1976043fb6c7cc1ce4d5ee8
dals83 <danni@magenta.dk>
2018-10-09 16:34:06 +0200

    * All scanner types are started the same way.

--- scrapy-webscanner/scanner_manager.py
+++ scrapy-webscanner/scanner_manager.py
@@ -3,15 +3,20 @@ import pika
 import json
 
 from run import ScannerApp
-from exchangescan.exchange_filescan import ExchangeFilescanner
 
 queue_name = 'datascanner'
 
 
-connection = pika.BlockingConnection(pika.ConnectionParameters('localhost', heartbeat_interval=6000))
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters('localhost',
+                              heartbeat_interval=6000)
+)
+
 channel = connection.channel()
 
 channel.queue_declare(queue=queue_name)
+
+scan_job_list = []
 
 
 def callback(ch, method, properties, body):
@@ -20,13 +25,8 @@ def callback(ch, method, properties, body):
     print(" [x] Received %r" % body)
     ch.basic_ack(delivery_tag=method.delivery_tag)
     # Collect scan object and map properties
-    if body['type'] == 'ExchangeScanner':
-        print('Starting exchange scanner.')
-        exchange_scanner = ExchangeFilescanner(body['id'])
-        exchange_scanner.start()
-    else:
-        scanner_app = ScannerApp(body['id'], body["logfile"])
-        scanner_app.start()
+    scan_job_list.append(ScannerApp(body['id'], body['type'], body["logfile"]))
+    scan_job_list[-1].start()
 
 
 channel.basic_consume(callback, queue=queue_name)
