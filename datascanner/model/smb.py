b3e0abcd059827c6139154c6330eb61bfe1e9709
Alexander Faithfull <af@magenta.dk>
2019-01-25 12:35:22 +0100

    Added support for generating and parsing smb: URLs

--- datascanner/model/smb.py
+++ datascanner/model/smb.py
@@ -2,12 +2,14 @@ from model.core import Source, Handle
 from model.file import FilesystemResource
 
 from os import rmdir
+from regex import compile, match
+from urllib.parse import quote, urlunsplit
 from pathlib import Path
 from tempfile import mkdtemp
 from subprocess import run
 
 class SMBSource(Source):
-    def __init__(self, unc, user='', password='', domain=''):
+    def __init__(self, unc, user=None, password=None, domain=None):
         self._unc = unc
         self._user = user
         self._password = password
@@ -58,6 +60,33 @@ class SMBSource(Source):
             for f in d.iterdir():
                 if f.is_file():
                     yield SMBHandle(self, f.relative_to(mntdir))
+
+    # Third form from https://www.iana.org/assignments/uri-schemes/prov/smb
+    def to_url(self):
+        server, path = self._unc.lstrip('/').split('/', maxsplit=1)
+        netloc = ""
+        if self._user:
+            if self._domain:
+                netloc += self._domain + ";"
+            netloc += self._user
+            if self._password:
+                netloc += ":" + self._password
+            netloc += "@"
+        netloc += server
+        return urlunsplit(('smb', netloc, quote(path), None, None))
+
+    netloc_regex = compile(r"^(((\w+);)?(\w+)(:(\w+))?@)?([\w.]+)$")
+    @staticmethod
+    def from_url(scheme, netloc, path):
+        match = SMBSource.netloc_regex.match(netloc)
+        if match:
+            _, _, domain, username, _, password, unc = match.groups()
+            return SMBSource("//" + unc + path,
+                username or None, password or None, domain or None)
+        else:
+            return None
+
+Source._register_url_handler("smb", SMBSource.from_url)
 
 class SMBHandle(Handle):
     def __init__(self, source, relpath):
