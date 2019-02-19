05d5b12ec1c358e130181320331d1b039802c00b
dals83 <danni@magent-aps.dk>
2019-02-19 15:18:04 +0100

    Now loads the django setup and starts a scan based on the type.

--- scrapy-webscanner/scanner_manager.py
+++ scrapy-webscanner/scanner_manager.py
@@ -2,7 +2,22 @@
 import pika
 import json
 
-from run import ScannerApp
+import os
+import sys
+import django
+
+base_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+sys.path.append(base_dir + "/webscanner_site")
+os.environ["DJANGO_SETTINGS_MODULE"] = "webscanner.settings"
+
+os.umask(0o007)
+os.environ["SCRAPY_SETTINGS_MODULE"] = "scanners.settings"
+
+django.setup()
+
+from run_webscan import StartWebScan
+from run_filescan import StartFileScan
+
 
 queue_name = 'datascanner'
 
@@ -25,9 +40,14 @@ def callback(ch, method, properties, body):
     print(" [x] Received %r" % body)
     ch.basic_ack(delivery_tag=method.delivery_tag)
     # Collect scan object and map properties
-    scan_job_list.append(ScannerApp(
-            body['id'], body['type'], body['logfile'], body['last_started']))
+    if body['type'] is 'Webscanner':
+        scan_job_list.append(StartWebScan(
+                body['id'], body['logfile'], body['last_started']))
+    else:
+        scan_job_list.append(StartFileScan(
+            body['id'], body['logfile'], body['last_started']))
     scan_job_list[-1].start()
+
 
 
 channel.basic_consume(callback, queue=queue_name)
