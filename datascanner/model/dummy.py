c385226b05002a70a92c4578f3cc998457fb2c4c
Alexander Faithfull <af@magenta.dk>
2019-02-19 17:21:42 +0100

    Added a new dummy Source for iterating over arbitrary Handles
    The use case here is something like WebHandle -- since WebSource doesn't
    actually work as a crawler yet, the new DummySource class can be used to
    emulate one based on a pre-prepared list of WebHandles

---
+++ datascanner/model/dummy.py
@@ -0,0 +1,9 @@
+from model.core import Source, Handle, Resource
+
+class DummySource(Source):
+    def __init__(self, *handles):
+        self._handles = handles
+
+    def handles(self, sm):
+        for h in self._handles:
+            yield h
