99241142b4e5dfbf36420da02be22af324186676
Alexander Faithfull <af@magenta.dk>
2019-02-25 16:52:57 +0100

    Processors should take Resources as arguments, not paths
    This allows processors that do clever things from within Python to avoid
    unnecessary disk I/O

--- datascanner/model/processors.py
+++ datascanner/model/processors.py
@@ -2,32 +2,33 @@
 
 from subprocess import run, PIPE, DEVNULL
 
-def plain_text_processor(f, **kwargs):
-    try:
-        with open(f, "rt") as t:
-            return t.read()
-    except UnicodeDecodeError:
-        return None
+def plain_text_processor(r, **kwargs):
+    with r.make_stream() as t:
+        try:
+            return t.read().decode()
+        except UnicodeDecodeError:
+            return None
 
-def image_processor(f, **kwargs):
-    return run(["tesseract", f, "stdout"],
-            timeout=2,
-            universal_newlines=True,
-            stdout=PIPE,
-            stderr=DEVNULL, **kwargs).stdout.strip()
+def image_processor(r, **kwargs):
+    with r.make_path() as f:
+        return run(["tesseract", f, "stdout"],
+                universal_newlines=True,
+                stdout=PIPE,
+                stderr=DEVNULL, **kwargs).stdout.strip()
 
-def pdf_processor(f, **kwargs):
-    return run(["pdftotext", f, "-"],
-            timeout=2,
-            universal_newlines=True,
-            stdout=PIPE,
-            stderr=DEVNULL, **kwargs).stdout.strip()
+def pdf_processor(r, **kwargs):
+    with r.make_path() as f:
+        return run(["pdftotext", f, "-"],
+                universal_newlines=True,
+                stdout=PIPE,
+                stderr=DEVNULL, **kwargs).stdout.strip()
 
-def html_processor(f, **kwargs):
-    return run(["html2text", f],
-            universal_newlines=True,
-            stdout=PIPE,
-            stderr=DEVNULL, **kwargs).stdout.strip()
+def html_processor(r, **kwargs):
+    with r.make_path() as f:
+        return run(["html2text", f],
+                universal_newlines=True,
+                stdout=PIPE,
+                stderr=DEVNULL, **kwargs).stdout.strip()
 
 processors = {
     "text/plain": plain_text_processor,
