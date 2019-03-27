d29a1db893a5cf5a3eb5a73784dee3e0a7af5550
Alexander Faithfull <af@magenta.dk>
2019-03-27 09:44:38 +0100

    Restructured the Python search path to allow code to be shared
    The principal packages of django-os2webscanner and scrapy-webscanner now
    include the repository root in their load path; this makes it possible for
    both of them to see the new os2datascanner_common module, which contains
    nothing at the moment (but which will soon!)

--- django-os2webscanner/os2webscanner/__init__.py
+++ django-os2webscanner/os2webscanner/__init__.py
@@ -1,1 +1,6 @@
 """Django module for the OS2datascanner project."""
+
+# Add the repository root to the load path
+from sys import path
+from os.path import dirname
+path.insert(0, dirname(__file__) + "/../..")
