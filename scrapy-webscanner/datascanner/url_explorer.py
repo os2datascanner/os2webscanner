7680b424533603cfb6c5f2d6a58f2432abc787d1
Alexander Faithfull <af@magenta.dk>
2019-05-09 14:10:55 +0200

    Fixed the indentation in url_explorer.py
    Argh! Two spaces per indentation level! That just won't do. This commit
    also moves the main body into an imaginatively-named main() function

--- scrapy-webscanner/datascanner/url_explorer.py
+++ scrapy-webscanner/datascanner/url_explorer.py
@@ -5,36 +5,36 @@ import argparse
 from .model.core import Source, SourceManager
 from .model.core import UnknownSchemeError
 
-guess_mime = True
+def print_source(manager, source, depth=0, guess=False):
+    for h in source.handles(manager):
+        print("{0}{1}".format("  " * depth, h))
+        derived_source = Source.from_handle(h, manager if not guess else None)
+        if derived_source:
+            with SourceManager(manager) as derived_manager:
+                print_source(derived_manager, derived_source, depth + 1, guess)
 
-def print_source(manager, source, depth=0):
-  for h in source.handles(manager):
-    print("{0}{1}".format("  " * depth, h))
-    derived_source = Source.from_handle(h, manager if not guess_mime else None)
-    if derived_source:
-      with SourceManager(manager) as derived_manager:
-        print_source(derived_manager, derived_source, depth + 1)
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+            "urls",
+            metavar="URL", nargs='+')
+    parser.add_argument(
+            "--compute-mime",
+            action='store_false', dest='guess')
+    parser.add_argument(
+            "--guess-mime",
+            action='store_true', dest='guess', default=True)
+
+    args = parser.parse_args()
+
+    sources = []
+    with SourceManager() as sm:
+        for i in args.urls:
+            try:
+                s = Source.from_url(i)
+                print_source(sm, s, guess=args.guess)
+            except UnknownSchemeError:
+                pass
 
 if __name__ == '__main__':
-  parser = argparse.ArgumentParser()
-  parser.add_argument(
-          "urls",
-          metavar="URL", nargs='+')
-  parser.add_argument(
-          "--compute-mime",
-          action='store_false', dest='guess')
-  parser.add_argument(
-          "--guess-mime",
-          action='store_true', dest='guess', default=True)
-
-  args = parser.parse_args()
-
-  guess_mime = args.guess
-  sources = []
-  with SourceManager() as sm:
-    for i in args.urls:
-      try:
-        s = Source.from_url(i)
-        print_source(sm, s)
-      except UnknownSchemeError:
-        pass
+    main()
