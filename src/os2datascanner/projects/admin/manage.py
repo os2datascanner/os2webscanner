ef00e18992371b9774a8d007c3974222116f702a
Jakob <jakob@magenta.dk>
2019-07-09 15:17:53 +0200

    Trying to fix python not being able to run manage

--- src/os2datascanner/projects/admin/manage.py
+++ src/os2datascanner/projects/admin/manage.py
@@ -6,7 +6,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "os2datascanner.projects.admin.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
     from django.core.management import execute_from_command_line
 
