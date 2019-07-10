c94a658c3d34c07ed7e9a43d6936b5caa9fb3d07
Jakob <jakob@magenta.dk>
2019-07-10 15:23:41 +0200

    fix paths in exchange_cron_script

--- contrib/cron/run_exchange_cron_script.sh
+++ contrib/cron/run_exchange_cron_script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-DIR=$(dirname "${BASH_SOURCE[0]}")
+DIR=$(dirname ${dirname "${BASH_SOURCE[0]}"})
 FULL_DIR="$(cd "$DIR" && pwd)"
 BASE_DIR=$(dirname "${FULL_DIR}")
 
 source "${BASE_DIR}/python-env/bin/activate"
-python "${BASE_DIR}/scrapy-webscanner/exchange_cron.py"
+python "${BASE_DIR}/src/os2datascanner/engine/exchange_cronjob.py"
