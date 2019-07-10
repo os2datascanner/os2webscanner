3b02a878f814167de65b21bb6bdc7a3d4025d680
Jakob <jakob@magenta.dk>
2019-07-10 09:40:34 +0200

    Revert "Re-added paths in wsgi file"
    This reverts commit 4da217222b3859820da465c96831ec89793ff020.

--- src/os2datascanner/projects/admin/wsgi.py
+++ src/os2datascanner/projects/admin/wsgi.py
@@ -8,26 +8,8 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
-import sys
-import pathlib
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "os2datascanner.projects.admin.settings")
-
-PROJECT_DIR = str(pathlib.Path(__file__).resolve().parent.parent.parent.parent.parent.absolute())
-
-lib_path = 'python-env/lib/python3.6/site-packages'
-lib_path1 = 'python-env/lib/python3.5/site-packages'
-
-lib_dir = os.path.join(PROJECT_DIR, lib_path)
-lib_dir1 = os.path.join(PROJECT_DIR, lib_path1)
-src_dir = os.path.join(PROJECT_DIR, 'src')
-
-sys.path[0:0] = [PROJECT_DIR, lib_dir, lib_dir1, src_dir]
-
-try:
-    import os2datascanner.projects.admin.adminapp
-except Exception:
-    raise RuntimeError("Path: " + str(sys.path))
 
 from django.core.wsgi import get_wsgi_application
 
