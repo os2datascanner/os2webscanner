92bd98eb54d5ff3879998d94124b6b914127e646
Jakob <jakob@magenta.dk>
2019-07-18 13:43:09 +0200

    Attempt to fix and restructure exchange and filescan

--- src/os2datascanner/engine/scanners/scanner_types/exchange_scanner.py
+++ src/os2datascanner/engine/scanners/scanner_types/exchange_scanner.py
@@ -1,7 +1,12 @@
 from .scanner import Scanner
-from .filescanner import FileScanner
+from .scanner_with_statistics import ScannerWithStatistics
 
 
-class ExchangeScanner(FileScanner):
+class ExchangeScanner(Scanner, ScannerWithStatistics):
     def get_domain_url(self):
         return self.get_scanner_object().dir_to_scan
+
+    def get_location_for(self, url):
+        p = as_path(url)
+
+        return super().get_location_for(p.as_uri())
--- src/os2datascanner/engine/scanners/scanner_types/filescanner.py
+++ src/os2datascanner/engine/scanners/scanner_types/filescanner.py
@@ -2,11 +2,12 @@ import pathlib
 
 from os2datascanner.engine.utils import as_path
 from .scanner import Scanner
+from .scanner_with_statistics import ScannerWithStatistics
 
 from os2datascanner.projects.admin.adminapp.models.statistic_model import Statistic, TypeStatistics
 
 
-class FileScanner(Scanner):
+class FileScanner(Scanner, ScannerWithStatistics):
     def get_domain_url(self):
         """Return a list of valid domain urls."""
         return self.get_scanner_object().mountpath
@@ -17,24 +18,3 @@ class FileScanner(Scanner):
         p = pathlib.PureWindowsPath(self.scan_object.webscanner.url) / p
 
         return super().get_location_for(p.as_uri())
-
-    def set_statistics(self,
-            supported_count, supported_size,
-            relevant_count, relevant_size,
-            relevant_unsupported_count, relevant_unsupported_size):
-        stats = Statistic.objects.get_or_create(scan=self.scan_object)[0]
-        stats.supported_count = supported_count
-        stats.supported_size = supported_size
-        stats.relevant_count = relevant_count
-        stats.relevant_size = relevant_size
-        stats.relevant_unsupported_count = relevant_unsupported_count
-        stats.relevant_unsupported_size = relevant_unsupported_size
-        stats.save()
-
-    def add_type_statistics(self, name, count, size):
-        stats = Statistic.objects.get_or_create(scan=self.scan_object)[0]
-        type_stats = TypeStatistics(statistic=stats)
-        type_stats.type_name = name
-        type_stats.count = count
-        type_stats.size = size
-        type_stats.save()
