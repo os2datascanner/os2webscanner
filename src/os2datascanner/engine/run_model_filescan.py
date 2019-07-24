dbe226b31801f150d77f33a972200f41cb9ff47c
Alexander Faithfull <af@magenta.dk>
2019-07-24 15:10:19 +0200

    Updated the engine2-based file scan to catch up with recent model changes
    Domains are gone and ConversionQueueItems are somewhere else now, and now
    that the code knows that, it can work again!

--- src/os2datascanner/engine/run_model_filescan.py
+++ src/os2datascanner/engine/run_model_filescan.py
@@ -30,57 +30,57 @@ class StartModelFileScan(StartScan, multiprocessing.Process):
 
         self.scanner = FileScanner(self.configuration)
         self.scanner.ensure_started()
+
+        mountpath = self.scanner.get_domain_url()
+        url = self.scanner.get_scanner_object().url
         try:
             with SourceManager() as sm:
-                for domain in self.scanner.valid_domains:
-                    domain = domain.filedomain
+                interesting = []
 
-                    interesting = []
+                all_size = 0
+                all_count = 0
 
-                    all_size = 0
-                    all_count = 0
+                interesting_size = 0
+                interesting_count = 0
 
-                    interesting_size = 0
-                    interesting_count = 0
+                logging.info("Exploring source...")
+                source = FilesystemSource(mountpath)
+                for h in source.handles(sm):
+                    mime = h.guess_type()
+                    stat = h.follow(sm).get_stat()
+                    all_size += stat.st_size
+                    all_count += 1
+                    if Processor.mimetype_to_processor_type(mime):
+                        interesting_size += stat.st_size
+                        interesting_count += 1
+                        interesting.append(h)
+                    if all_count % 1000 == 0:
+                        logging.info("Exploring: found {0} files, {1} of which interesting...".format(all_count, interesting_count))
+                logging.info("Exploration complete: found {0} files ({1} bytes), of which {2} ({3} bytes) interesting.".format(all_count, all_size, interesting_count, interesting_size))
 
-                    logging.info("Exploring source...")
-                    source = FilesystemSource(domain.mountpath)
-                    for h in source.handles(sm):
-                        mime = h.guess_type()
-                        stat = h.follow(sm).get_stat()
-                        all_size += stat.st_size
-                        all_count += 1
-                        if Processor.mimetype_to_processor_type(mime):
-                            interesting_size += stat.st_size
-                            interesting_count += 1
-                            interesting.append(h)
-                        if all_count % 1000 == 0:
-                            logging.info("Exploring: found {0} files, {1} of which interesting...".format(all_count, interesting_count))
-                    logging.info("Exploration complete: found {0} files ({1} bytes), of which {2} ({3} bytes) interesting.".format(all_count, all_size, interesting_count, interesting_size))
+                self.scanner.set_statistics(
+                        supported_size=interesting_size,
+                        supported_count=interesting_count,
+                        relevant_size=all_size,
+                        relevant_count=all_count,
+                        relevant_unsupported_size=0,
+                        relevant_unsupported_count=0)
 
-                    self.scanner.set_statistics(
-                            supported_size=interesting_size,
-                            supported_count=interesting_count,
-                            relevant_size=all_size,
-                            relevant_count=all_count,
-                            relevant_unsupported_size=0,
-                            relevant_unsupported_count=0)
+                logging.info("Fetching interesting items...")
+                for idx, h in enumerate(interesting):
+                    url = urljoin(source.to_url() + "/",
+                            str(h.get_relative_path()))
+                    url = url.replace(mountpath, url)
+                    logging.info("Item {0}/{1}: {2}".format(idx + 1, interesting_count, url))
 
-                    logging.info("Fetching interesting items...")
-                    for idx, h in enumerate(interesting):
-                        url = urljoin(source.to_url() + "/",
-                                str(h.get_relative_path()))
-                        url = url.replace(domain.mountpath, domain.url)
-                        logging.info("Item {0}/{1}: {2}".format(idx + 1, interesting_count, url))
-
-                        url_object = self.scanner.mint_url(
-                                url=url, mime_type=h.guess_type())
-                        with h.follow(sm).make_stream() as s:
-                            self.scanner.scan(s.read(), url_object)
-                    logging.info("Fetched interesting items.")
+                    url_object = self.scanner.mint_url(
+                            url=url, mime_type=h.guess_type())
+                    with h.follow(sm).make_stream() as s:
+                        self.scanner.scan(s.read(), url_object)
+                logging.info("Fetched interesting items.")
 
             logging.info("Waiting for processing to finish...")
-            from os2datascanner.sites.admin.adminapp.models.conversionqueueitem_model import ConversionQueueItem
+            from os2datascanner.projects.admin.adminapp.models.conversionqueueitem_model import ConversionQueueItem
             while True:
                 remaining_queue_items = ConversionQueueItem.objects.filter(
                     status__in=[ConversionQueueItem.NEW,
