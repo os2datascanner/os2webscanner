5a17c53220ab7da3e924dcbe9b1d7d0c24e3b858
Alexander Faithfull <af@magenta.dk>
2019-07-30 16:18:32 +0200

    Added resource summary support to url_explorer.py
    Passing the new --summarise option will print, along with each Handle, its
    size, date of last modification and computed MIME type

--- src/os2datascanner/engine2/demo/url_explorer.py
+++ src/os2datascanner/engine2/demo/url_explorer.py
@@ -3,15 +3,31 @@
 import argparse
 
 from ..model.core import Source, SourceManager
-from ..model.core import UnknownSchemeError
+from ..model.core import UnknownSchemeError, ResourceUnavailableError
 
-def print_source(manager, source, depth=0, guess=False):
-    for h in source.handles(manager):
-        print("{0}{1}".format("  " * depth, h))
-        derived_source = Source.from_handle(h, manager if not guess else None)
+def format_d(depth, fmt, *args, **kwargs):
+    return "{0}{1}".format("  " * depth, fmt.format(*args, **kwargs))
+
+def print_source(manager, source, depth=0, guess=False, summarise=False):
+    for handle in source.handles(manager):
+        print(format_d(depth, "{0}", handle))
+        if summarise:
+            resource = handle.follow(manager)
+            try:
+                size = resource.get_size()
+                mime = resource.compute_type()
+                lm = resource.get_last_modified()
+                print(format_d(depth + 1, "size {0} bytes", size))
+                print(format_d(depth + 1, "type {0}", mime))
+                print(format_d(depth + 1, "lmod {0}", lm))
+            except ResourceUnavailableError as ex:
+                print(format_d(depth + 1, "not available: {0}", ex.args[1:]))
+        derived_source = Source.from_handle(
+                handle, manager if not guess else None)
         if derived_source:
             with SourceManager(manager) as derived_manager:
-                print_source(derived_manager, derived_source, depth + 1, guess)
+                print_source(derived_manager,
+                        derived_source, depth + 1, guess, summarise)
 
 def main():
     parser = argparse.ArgumentParser()
@@ -22,6 +38,9 @@ def main():
             "--compute-mime",
             action='store_false', dest='guess')
     parser.add_argument(
+            "--summarise",
+            action='store_true', dest='summarise')
+    parser.add_argument(
             "--guess-mime",
             action='store_true', dest='guess', default=True)
 
@@ -31,7 +50,7 @@ def main():
         for i in args.urls:
             try:
                 s = Source.from_url(i)
-                print_source(sm, s, guess=args.guess)
+                print_source(sm, s, guess=args.guess, summarise=args.summarise)
             except UnknownSchemeError:
                 pass
 
