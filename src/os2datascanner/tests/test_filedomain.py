2e731e52f2b434ecd8a896f213e931e3190536cb
Dan Villiom Podlaski Christiansen <dan@magenta.dk>
2019-08-01 17:03:12 +0200

    make gitlab run all the tests
    - ensure that test file names start with "test"
    - just specify the toplevel package

---
+++ src/os2datascanner/tests/test_filedomain.py
@@ -0,0 +1,51 @@
+from django.contrib.auth.models import User
+from django.test import TestCase, RequestFactory
+
+from ...test.util import CreateOrganization, CreateFileDomain
+from ...models.domain_model import Domain
+from ...models.filedomain_model import FileDomain
+
+
+class FileDomainTest(TestCase):
+    """Unit-tests for the Create File Domain Form."""
+
+    fields = [
+        ["url"],
+        ["organization"],
+        ["username"],
+        ["password"],
+        ["validation_status"],
+        ["exclusion_rules"]
+    ]
+
+    def setUp(self):
+        self.factory = RequestFactory()
+        CreateOrganization.create_organization(self)
+        self.user = User.objects.create_user(
+            username='jacob', email='jacob@magenta.dk', password='top_secret')
+        # use models.userprofile_model
+
+    def test_view_filedomains(self):
+        self.client.login(username='jacob', password='top_secret')
+        response = self.client.get('/filedomains/', follow=True)
+        self.assertEqual(response.status_code, 200)
+
+    def test_filedomain_inheritance(self):
+        filedomain = CreateFileDomain.create_filedomain(self)
+        domain = Domain.objects.get()
+        domain = domain.filedomain
+        self.assertEqual(filedomain.url, domain.url)
+        self.assertEqual(isinstance(domain, FileDomain), True)
+
+    """def test_create_filedomains(self):
+        self.client.login(username='jacob', password='top_secret')
+        form_data = {
+            'url': 'something.test',
+            'organization': 'magenta',
+            'username': 'mette',
+            'password': 'projektleder',
+            'validation_status': '',
+            'exclusion_rules': ''
+        }
+        response = self.client.post('/filedomains/add/', form_data, follow=True)
+        self.assertEqual(response.status_code, 200)"""
