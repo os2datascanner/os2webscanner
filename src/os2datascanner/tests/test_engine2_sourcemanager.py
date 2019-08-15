a9e3cdf45557b2c1eaeca7e53437023c5a358082
Alexander Faithfull <af@magenta.dk>
2019-08-15 17:05:04 +0200

    Added tests for SourceManager sharing and cookie deduplication

---
+++ src/os2datascanner/tests/test_engine2_sourcemanager.py
@@ -0,0 +1,64 @@
+import unittest
+import contextlib
+from os import stat
+
+from os2datascanner.engine2.model.core import (
+        Source, ShareableCookie, SourceManager, UnknownSchemeError,
+        ResourceUnavailableError)
+from os2datascanner.engine2.model.http import WebSource, WebHandle
+
+
+magenta = WebSource("https://www.magenta.dk/")
+
+
+class Tracker:
+    special_cookie = object()
+
+    def __init__(self):
+        self.count = 0
+
+    def _open(self, sm):
+        self.count += 1
+        return self.special_cookie
+
+    def _close(self, cookie):
+        self.count -= 1
+
+
+class ShareableTracker(Tracker):
+    special_cookie = ShareableCookie(object())
+
+
+class Engine2SourceManagerTest(unittest.TestCase):
+    def test_opening(self):
+        tracker = Tracker()
+        with SourceManager() as sm:
+            sm.open(tracker)
+            sm.open(tracker)
+            self.assertEqual(
+                    tracker.count,
+                    1,
+                    "SourceManager opened the same object twice")
+
+    def test_sharing(self):
+        tracker1 = Tracker()
+        tracker2 = ShareableTracker()
+
+        with SourceManager() as sm:
+            cookie1 = sm.open(tracker1)
+            cookie2 = sm.open(tracker2)
+
+            shared = sm.share()
+            self.assertNotEqual(
+                    cookie1,
+                    shared.open(tracker1, try_open=False),
+                    "shared SourceManager contained non-shareable cookie")
+            self.assertEqual(
+                    cookie2,
+                    shared.open(tracker2, try_open=False),
+                    "shared SourceManager did not contain shareable cookie")
+
+    def test_read_only(self):
+        with self.assertRaises(TypeError):
+            with SourceManager().share() as sm:
+                sm.open(tracker)
