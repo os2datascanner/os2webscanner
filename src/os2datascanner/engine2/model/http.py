os2datascanner/os2webscanner

commit 1c142d27e622b6dfbec3a57f90c088c53e784ab5

Drastically simplified Source's state allocation and deallocation protocol
This commit replaces the _open and _close methods with a new
_generate_state generator. This makes a lot of the code markedly cleaner --
in particular, Sources based on Handles are much less of a hack

src/os2datascanner/engine2/model/http.py
@@ -24,11 +24,9 @@ class WebSource(Source):
     def __str__(self):
         return "WebSource({0})".format(self._url)
 
-    def _open(self, sm):
-        return Session()
-
-    def _close(self, session):
-        session.close()
+    def _generate_state(self, sm):
+        with requests.Session() as session:
+            yield session
 
     def handles(self, sm):
         session = sm.open(self)

src/os2datascanner/engine2/model/core.py
@@ -20,19 +20,16 @@ class Source(ABC, _TypPropEq):
     only opening one of them.)"""
 
     @abstractmethod
-    def _open(self, sm):
-        """Opens this Source in the given SourceManager. Returns a cookie of
-        some kind that can be used to interact with the opened state, and which
-        SourceManager will later pass to _close.
+    def _generate_state(self, sm):
+        """Returns a state management generator. This generator will only be
+        executed once: this will yield a magic cookie representing any state
+        that this Source might require. The generator will be closed when
+        this state is no longer needed.
 
         The relevant instance properties when considering Source equality are
         normally only those properties used by this method. (The default
         implementation is conservative, however, and compares all
         properties.)"""
-
-    @abstractmethod
-    def _close(self, cookie):
-        """Closes a cookie previously returned by _open."""
 
     @abstractmethod
     def handles(self, sm):
@@ -179,12 +176,13 @@ class SourceManager:
             if self._parent:
                 cookie = self._parent.open(source, try_open=False)
             if not cookie and try_open:
-                cookie = source._open(self)
+                generator = source._generate_state(self)
+                cookie = next(generator)
                 self._order.append(source)
-                self._opened[source] = cookie
+                self._opened[source] = (generator, cookie)
             rv = cookie
         else:
-            rv = self._opened[source]
+            _, rv = self._opened[source]
         if isinstance(rv, ShareableCookie):
             return rv.get()
         else:
@@ -201,10 +199,8 @@ class SourceManager:
         this SourceManager."""
         try:
             for k in reversed(self._order):
-                cookie = self._opened[k]
-                if isinstance(cookie, ShareableCookie):
-                    cookie = cookie.get()
-                k._close(cookie)
+                generator, _ = self._opened[k]
+                generator.close()
         finally:
             self._order = []
             self._opened = {}
@@ -215,7 +211,7 @@ class ShareableCookie:
     operation on a remote drive, or a connection to a server, or even nothing
     at all.
 
-    The Source._open function can return a ShareableCookie to indicate that a
+    Source._generate_state can yield a ShareableCookie to indicate that a
     cookie can (for the duration of its SourceManager's context) meaningfully
     be shared across processes, because the operations that it has performed
     are not specific to a single process.

src/os2datascanner/engine2/model/data.py
@@ -18,11 +18,8 @@ class DataSource(Source):
     def __str__(self):
         return "DataSource(content=..., mime={0})".format(self._mime)
 
-    def _open(self, sm):
-        return EMPTY_COOKIE
-
-    def _close(self, sm):
-        pass
+    def _generate_state(self, sm):
+        yield EMPTY_COOKIE
 
     def to_url(self):
         return "data:{0};base64,{1}".format(self._mime, b64encode(self._content).decode(encoding='ascii'))

src/os2datascanner/engine2/model/file.py
@@ -25,11 +25,8 @@ class FilesystemSource(Source):
     def __str__(self):
         return "FilesystemSource({0})".format(self._path)
 
-    def _open(self, sm):
-        return ShareableCookie(self._path)
-
-    def _close(self, sm):
-        pass
+    def _generate_state(self, sm):
+        yield ShareableCookie(self._path)
 
     def to_url(self):
         return urlunsplit(('file', '', quote(str(self._path)), None, None))

src/os2datascanner/engine2/model/filtered.py
@@ -21,11 +21,8 @@ class FilteredSource(Source):
         rest, ext = os.path.splitext(self._handle.get_name())
         yield FilteredHandle(self, rest)
 
-    def _open(self, sm):
-        return self._handle.follow(sm)
-
-    def _close(self, cookie):
-        pass
+    def _generate_state(self, sm):
+        yield self._handle.follow(sm)
 
 @Source.mime_handler("application/gzip")
 def _gzip(handle):

src/os2datascanner/engine2/model/smb.py
@@ -34,17 +34,19 @@ class SMBSource(Source):
     def __str__(self):
         return "SMBSource({0}, {1})".format(self._unc, self._make_optarg())
 
-    def _open(self, sm):
+    def _generate_state(self, sm):
         mntdir = mkdtemp()
         try:
             args = ["mount", "-t", "cifs", self._unc, mntdir, '-o']
             args.append(self._make_optarg(display=False))
             print(args)
             assert run(args).returncode == 0
-            return ShareableCookie(mntdir)
-        except:
+
+            yield ShareableCookie(mntdir)
+
+            assert run(["umount", mntdir]).returncode == 0
+        finally:
             rmdir(mntdir)
-            raise
 
     def _close(self, mntdir):
         args = ["umount", mntdir]

src/os2datascanner/engine2/model/smbc.py
@@ -31,13 +31,9 @@ class SMBCSource(Source):
         return "SMBCSource({0}, {1}, ****, {2})".format(
                 self._unc, self._user, self._domain)
 
-    def _open(self, sm):
-        context = smbc.Context()
-        return (self._to_url(), context)
-
-    def _close(self, cookie):
-        # There seems to be no way to shut down a context
-        pass
+    def _generate_state(self, sm):
+        yield (self._to_url(), smbc.Context())
+        # There seems to be no way to shut down a context...
 
     def handles(self, sm):
         url, context = sm.open(self)

src/os2datascanner/engine2/model/tar.py
@@ -14,20 +14,15 @@ class TarSource(Source):
         return "TarSource({0})".format(self._handle)
 
     def handles(self, sm):
-        _, tarfile = sm.open(self)
+        tarfile = sm.open(self)
         for f in tarfile.getmembers():
             if f.isfile():
                 yield TarHandle(self, f.name)
 
-    def _open(self, sm):
-        r = self._handle.follow(sm).make_path()
-        path = r.__enter__()
-        return (r, open_tar(str(path), "r"))
-
-    def _close(self, cookie):
-        r, tarfile = cookie
-        r.__exit__(None, None, None)
-        tarfile.close()
+    def _generate_state(self, sm):
+        with self._handle.follow(sm).make_path() as r:
+            with open_tar(str(r), "r") as tp:
+                yield tp
 
 class TarHandle(Handle):
     def follow(self, sm):
@@ -40,7 +35,7 @@ class TarResource(FileResource):
 
     def get_info(self):
         if not self._info:
-            self._info = self._open_source()[1].getmember(
+            self._info = self._open_source().getmember(
                     self.get_handle().get_relative_path())
         return self._info
 
@@ -66,7 +61,7 @@ class TarResource(FileResource):
 
     @contextmanager
     def make_stream(self):
-        with self._open_source()[1].extractfile(
+        with self._open_source().extractfile(
                  self.get_handle().get_relative_path()) as s:
             yield s
 

commit d047fa73b6dabd0776e6e0de40ca70eb9bcd5d8d

Fixed an invalid reference in http.py
Commit 1c142d27e622b6dfbec3a57f90c088c53e784ab5 accidentally changed the
correct usage of "Session" to the incorrect "requests.Session"; this commit
undoes that mistake

src/os2datascanner/engine2/model/http.py
@@ -27,7 +27,7 @@ class WebSource(Source):
         return "WebSource({0})".format(self._url)
 
     def _generate_state(self, sm):
-        with requests.Session() as session:
+        with Session() as session:
             yield session
 
     def handles(self, sm):