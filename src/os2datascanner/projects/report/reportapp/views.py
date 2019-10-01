c359e0889b246e105000b525e0bfd783a62541c4
Steffen Jørgensen <sj@magenta-aps.dk>
2019-10-01 09:46:23 +0200

    linting

--- src/os2datascanner/projects/report/reportapp/views.py
+++ src/os2datascanner/projects/report/reportapp/views.py
@@ -2,30 +2,37 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import DocumentReport
 from .serializers import DocumentReportSerializer
-from django.views.generic import TemplateView 
+from django.views.generic import TemplateView
 
 # Create your views here.
 class ReportView(generics.ListAPIView):
-  queryset = DocumentReport.objects.all()
-  serializer_class = DocumentReportSerializer
+    queryset = DocumentReport.objects.all()
+    serializer_class = DocumentReportSerializer
+
 
 class LoginPageView(TemplateView):
-  template_name = 'login.html'
+    template_name = 'login.html'
+
 
 class MainPageView(TemplateView):
-  template_name = 'index.html'
+    template_name = 'index.html'
+
 
 class RulePageView(TemplateView):
-  template_name = 'rule.html'
+    template_name = 'rule.html'
+
 
 class ApprovalPageView(TemplateView):
-  template_name = 'approval.html'
+    template_name = 'approval.html'
+
 
 class StatsPageView(TemplateView):
-  template_name = 'stats.html'
+    template_name = 'stats.html'
+
 
 class SettingsPageView(TemplateView):
-  template_name = 'settings.html'
+    template_name = 'settings.html'
+
 
 class AboutPageView(TemplateView):
-  template_name = 'about.html'
+    template_name = 'about.html'
