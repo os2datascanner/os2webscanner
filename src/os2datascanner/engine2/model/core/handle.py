96fd532b7f3850722b450a56e74e27058c5ec5b7
Alexander Faithfull <af@magenta.dk>
2019-10-09 15:19:52 +0200

    Split up engine2's core.py file into a number of smaller modules
    This change should (hopefully!) be invisible to the outside world thanks
    to some __all__ trickery in the new core/__init__.py file

---
+++ src/os2datascanner/engine2/model/core/handle.py
@@ -0,0 +1,106 @@
+import os.path
+from abc import ABC, abstractmethod
+from mimetypes import guess_type
+
+from .source import Source
+from .utilities import _TypPropEq
+
+class Handle(ABC, _TypPropEq):
+    """A Handle is a reference to a leaf node in a hierarchy maintained by a
+    Source. Handles can be followed to give a Resource, a concrete object.
+
+    Although all Handle subclasses expose the same two-argument constructor,
+    which takes a Source and a string representation of a path, each type of
+    Source defines what its Handles and their paths mean; the only general way
+    to get a meaningful Handle is the Source.handles() function (or to make a
+    copy of an existing one).
+
+    Handles are serialisable and persistent, and two different Handles with the
+    same type and properties compare equal."""
+
+    type_label = None
+    """A label that will be used to identify JSON forms of this Handle."""
+
+    def __init__(self, source, relpath):
+        self._source = source
+        self._relpath = relpath
+
+    def get_source(self):
+        """Returns this Handle's Source."""
+        return self._source
+
+    def get_relative_path(self):
+        """Returns this Handle's path."""
+        return self._relpath
+
+    def get_name(self):
+        """Returns the base name -- everything after the last '/' -- of this
+        Handle's path, or "file" if the result would otherwise be empty."""
+        return os.path.basename(self._relpath) or 'file'
+
+    def guess_type(self):
+        """Guesses the type of this Handle's target based on its name. (For a
+        potentially better, but more expensive, guess, follow this Handle to
+        get a Resource and call its compute_type() method instead.)"""
+        mime, _ = guess_type(self.get_name())
+        return mime or "application/octet-stream"
+
+    def __str__(self):
+        return "{0}({1}, {2})".format(
+                type(self).__name__, self._source, self._relpath)
+
+    @abstractmethod
+    def follow(self, sm):
+        """Follows this Handle using the state in the StateManager @sm,
+        returning a concrete Resource."""
+
+    BASE_PROPERTIES = ('_source', '_relpath',)
+    """The properties defined by Handle. (If a subclass defines other
+    properties, but wants those properties to be ignored when comparing
+    objects, it should set the 'eq_properties' class attribute to this
+    value.)"""
+
+    def to_json_object(self):
+        """Returns an object suitable for JSON serialisation that represents
+        this Handle."""
+        return {
+            "type": self.type_label,
+            "source": self.get_source().to_json_object(),
+            "path": self.get_relative_path()
+        }
+
+    __json_handlers = {}
+    @staticmethod
+    def json_handler(type_label):
+        """Decorator: registers the decorated function as the handler for the
+        type label given as an argument. This handler will be called by
+        from_json_object when it finds this type label.
+
+        Subclasses should use this method to decorate their from_json_object
+        factory methods."""
+        def _json_handler(func):
+            if type_label in Handle.__json_handlers:
+                raise ValueError(
+                        "BUG: can't register two handlers" +
+                        " for the same JSON type label!", type_label)
+            Handle.__json_handlers[type_label] = func
+            return func
+        return _json_handler
+
+    @staticmethod
+    def stock_json_handler(type_label, constructor):
+        return Handle.json_handler(type_label)(lambda obj: constructor(
+                Source.from_json_object(obj["source"]), obj["path"]))
+
+    @staticmethod
+    def from_json_object(obj):
+        """Converts a JSON representation of a Handle, as returned by the
+        Handle.to_json_object method, back into a Handle."""
+        try:
+            tl = obj["type"]
+            if not tl in Handle.__json_handlers:
+                raise UnknownSchemeError(tl)
+            return Handle.__json_handlers[tl](obj)
+        except KeyError as k:
+            tl = obj.get("type", None)
+            raise DeserialisationError(tl, k.args[0])
---
+++ src/os2datascanner/engine2/model/core/source.py
@@ -0,0 +1,163 @@
+from abc import ABC, abstractmethod
+
+from .errors import UnknownSchemeError, DeserialisationError
+from .utilities import _TypPropEq
+
+class Source(ABC, _TypPropEq):
+    """A Source represents the root of a hierarchy to be explored. It
+    constructs Handles, which represent the position of an object in the
+    hierarchy.
+
+    Sources hold all the information (if any) needed to open a connection to
+    their hierarchy, but they aren't responsible for holding actual connection
+    state -- that gets stashed in a SourceManager instead.
+
+    Sources are serialisable and persistent, and two different Source objects
+    with the same type and properties compare equal. (One useful consequence of
+    this is that SourceManager will collapse several equal Sources together,
+    only opening one of them.)"""
+
+    type_label = None
+    """A label that will be used to identify JSON forms of this Source."""
+
+    @abstractmethod
+    def _generate_state(self, sm):
+        """Returns a state management generator. This generator will only be
+        executed once: this will yield a magic cookie representing any state
+        that this Source might require. The generator will be closed when
+        this state is no longer needed.
+
+        The relevant instance properties when considering Source equality are
+        normally only those properties used by this method. (The default
+        implementation is conservative, however, and compares all
+        properties.)"""
+
+    @abstractmethod
+    def handles(self, sm):
+        """Yields Handles corresponding to every identifiable leaf node in this
+        Source's hierarchy. These Handles are generated in an undefined order.
+
+        Note that this function can yield Handles that correspond to
+        identifiable *but non-existent* leaf nodes. These might correspond to,
+        for example, a broken link on a web page, or to an object that was
+        yielded by this function but was deleted before it could be examined.
+        These Handles can be detected by catching the ResourceUnavailableError
+        exception.
+
+        It is not necessarily the case that the result of the get_source call
+        on a Handle yielded by this function will be this Source."""
+
+    __url_handlers = {}
+    @staticmethod
+    def url_handler(*schemes):
+        """Decorator: registers the decorated function as the handler for the
+        URL schemes given as arguments. This handler will be called by from_url
+        when it finds one of these schemes.
+
+        Subclasses should use this decorator to register their from_url factory
+        methods."""
+        def _url_handler(func):
+            for scheme in schemes:
+                if scheme in Source.__url_handlers:
+                    raise ValueError(
+                            "BUG: can't register two handlers" +
+                            " for the same URL scheme!", scheme)
+                Source.__url_handlers[scheme] = func
+            return func
+        return _url_handler
+
+    @staticmethod
+    def from_url(url):
+        """Parses the given URL to produce a new Source."""
+        try:
+            scheme, _ = url.split(':', maxsplit=1)
+            if not scheme in Source.__url_handlers:
+                raise UnknownSchemeError(scheme)
+            return Source.__url_handlers[scheme](url)
+        except ValueError:
+            raise UnknownSchemeError()
+
+    # There is no general requirement that subclasses implement a to_url
+    # method (what's the URL of a file in a deeply-nested archive?), but many
+    # of them do. If a Source provides a to_url method, it is a requirement
+    # that Source.from_url(Source.to_url(src)) == src.
+
+    __mime_handlers = {}
+    @staticmethod
+    def mime_handler(*mimes):
+        """Decorator: registers the decorated function as the handler for the
+        MIME types given as arguments. This handler will be called by
+        from_handle when it finds one of these MIME types.
+
+        Subclasses should use this decorator to register their from_handle
+        factory methods, if they implement such a method."""
+        def _mime_handler(func):
+            for mime in mimes:
+                if mime in Source.__mime_handlers:
+                    raise ValueError(
+                            "BUG: can't register two handlers" +
+                            " for the same MIME type!", mime)
+                Source.__mime_handlers[mime] = func
+            return func
+        return _mime_handler
+
+    @staticmethod
+    def from_handle(handle, sm=None):
+        """Tries to create a Source from a Handle.
+
+        This will only work if the target of the Handle in question can
+        meaningfully be interpreted as the root of a hierarchy of its own --
+        for example, if it's an archive."""
+        if not sm:
+            mime = handle.guess_type()
+        else:
+            mime = handle.follow(sm).compute_type()
+        if mime in Source.__mime_handlers:
+            return Source.__mime_handlers[mime](handle)
+        else:
+            return None
+
+    def to_handle(self):
+        """If this Source was created based on a Handle (typically by the
+        Source.from_handle function), then returns that Handle; otherwise,
+        returns None."""
+        return None
+
+    @abstractmethod
+    def to_json_object(self):
+        """Returns an object suitable for JSON serialisation that represents
+        this Source."""
+        return {
+            "type": self.type_label
+        }
+
+    __json_handlers = {}
+    @staticmethod
+    def json_handler(type_label):
+        """Decorator: registers the decorated function as the handler for the
+        type label given as an argument. This handler will be called by
+        from_json_object when it finds this type label.
+
+        Subclasses should use this decorator to register their from_json_object
+        factory methods."""
+        def _json_handler(func):
+            if type_label in Source.__json_handlers:
+                raise ValueError(
+                        "BUG: can't register two handlers" +
+                        " for the same JSON type label!", type_label)
+            Source.__json_handlers[type_label] = func
+            return func
+        return _json_handler
+
+    @staticmethod
+    def from_json_object(obj):
+        """Converts a JSON representation of a Source, as returned by the
+        Source.to_json_object method, back into a Source."""
+        try:
+            tl = obj["type"]
+            if not tl in Source.__json_handlers:
+                raise UnknownSchemeError(tl)
+            return Source.__json_handlers[tl](obj)
+        except KeyError as k:
+            tl = obj.get("type", None)
+            raise DeserialisationError(tl, k.args[0])
