5a7d2b97bb778372a9cd99b4c809bf0d111713c7
Alexander Faithfull <af@magenta.dk>
2019-10-22 13:21:29 +0200

    Added a test to check that JSON serialisation of Rules works properly
    For laziness's sake, this reuses the same rule objects that
    test_compound_rule_matches was already using

--- src/os2datascanner/tests/test_engine2_rules.py
+++ src/os2datascanner/tests/test_engine2_rules.py
@@ -17,7 +17,7 @@ class RuleTests(unittest.TestCase):
                 [
                     "2205XXXXXX",
                     "2305XXXXXX",
-                    "2405XXXXXX"             
+                    "2405XXXXXX"
                 ]
             ),
             (
@@ -28,7 +28,7 @@ class RuleTests(unittest.TestCase):
 240501-0006: forbryder""",
                 [
                     "2305XXXXXX",
-                    "2405XXXXXX"             
+                    "2405XXXXXX"
                 ]
             ),
             (
@@ -58,43 +58,43 @@ more!""",
                 self.assertEqual(
                         [match["match"] for match in matches], expected)
 
-    def test_compound_rule_matches(self):
-        candidates = [
-            (
+    compound_candidates = [
+        (
+            AndRule(
+                RegexRule("A"),
+                OrRule(
+                    RegexRule("B"),
+                    RegexRule("C")
+                )
+            ), [
+                ("A", False, 3),
+                ("AB", True, 2),
+                ("ABC", True, 2),
+                ("BC", False, 1),
+                ("AC", True, 3)
+            ]
+        ),
+        (
+            NotRule(
                 AndRule(
                     RegexRule("A"),
                     OrRule(
                         RegexRule("B"),
                         RegexRule("C")
                     )
-                ), [
-                    ("A", False, 3),
-                    ("AB", True, 2),
-                    ("ABC", True, 2),
-                    ("BC", False, 1),
-                    ("AC", True, 3)
-                ]
-            ),
-            (
-                NotRule(
-                    AndRule(
-                        RegexRule("A"),
-                        OrRule(
-                            RegexRule("B"),
-                            RegexRule("C")
-                        )
-                    )
-                ), [
-                    ("A", True, 3),
-                    ("AB", False, 2),
-                    ("ABC", False, 2),
-                    ("BC", True, 1),
-                    ("AC", False, 3)
-                ]
-            )
-        ]
+                )
+            ), [
+                ("A", True, 3),
+                ("AB", False, 2),
+                ("ABC", False, 2),
+                ("BC", True, 1),
+                ("AC", False, 3)
+            ]
+        )
+    ]
 
-        for rule, tests in candidates:
+    def test_compound_rule_matches(self):
+        for rule, tests in RuleTests.compound_candidates:
             for input_string, outcome, evaluation_count in tests:
                 now = rule
                 evaluations = 0
@@ -121,3 +121,14 @@ more!""",
                         evaluation_count,
                         evaluations,
                         "{0}: wrong evaluation count".format(input_string))
+
+    def test_json_round_trip(self):
+        for rule, _ in RuleTests.compound_candidates:
+            with self.subTest(rule):
+                json = rule.to_json_object()
+                back_again = rule.from_json_object(json)
+                print(rule)
+                print(json)
+                print(back_again)
+                self.assertEqual(rule, back_again)
+                print("--")
