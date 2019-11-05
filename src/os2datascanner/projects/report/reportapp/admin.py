608a0e60c41357a22b511fe667898cf229044e77
Alexander Faithfull <af@magenta.dk>
2019-11-05 15:40:15 +0100

    Introduced a models hierarchy in the report module
    The alias models will move over to this module in a moment, and they'll
    need to be put somewhere

--- src/os2datascanner/projects/report/reportapp/admin.py
+++ src/os2datascanner/projects/report/reportapp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import DocumentReport
+from .models.documentreport_model import DocumentReport
 # Register your models here.
 
 admin.site.register(DocumentReport)
---
+++ src/os2datascanner/projects/report/reportapp/models/__init__.py
@@ -0,0 +1,1 @@
+from . import documentreport_model # noqa
