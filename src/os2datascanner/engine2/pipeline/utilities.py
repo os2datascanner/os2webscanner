19c47cb99d4648b462de8223d617d3c1ca43a1e9
Alexander Faithfull <af@magenta.dk>
2019-11-27 13:53:11 +0100

    Added a new Prometheus summary decorator to the pipeline utility module

--- src/os2datascanner/engine2/pipeline/utilities.py
+++ src/os2datascanner/engine2/pipeline/utilities.py
@@ -6,6 +6,7 @@ if systemd.daemon.booted():
 else:
     def sd_notify(status):
         return False
+from prometheus_client import Summary
 
 
 def make_common_argument_parser():
@@ -37,6 +38,15 @@ def notify_status(msg):
 
 def notify_watchdog():
     sd_notify("WATCHDOG=1")
+
+
+def prometheus_summary(*args):
+    """Decorator. Records a Prometheus summary observation for every call to
+    the decorated function."""
+    s = Summary(*args)
+    def _prometheus_summary(func):
+        return s.time()(func)
+    return _prometheus_summary
 
 
 def json_event_processor(listener):
