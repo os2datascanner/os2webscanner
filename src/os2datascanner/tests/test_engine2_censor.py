cd68a0941528d3c32cd0eb9ba95eccb591507bb2
Alexander Faithfull <af@magenta.dk>
2019-12-04 12:05:24 +0100

    Added some simple tests of engine2 censoring
    This doesn't test everything, of course, but it makes sure that the basics
    are in place

---
+++ src/os2datascanner/tests/test_engine2_censor.py
@@ -0,0 +1,46 @@
+import unittest
+
+from os2datascanner.engine2.model.ews import (
+        EWSMailHandle, EWSAccountSource, OFFICE_365_ENDPOINT as CLOUD)
+from os2datascanner.engine2.model.smb import SMBSource, SMBHandle
+from os2datascanner.engine2.model.smbc import SMBCSource, SMBCHandle
+from os2datascanner.engine2.model.zip import ZipSource, ZipHandle
+
+
+class CensorTests(unittest.TestCase):
+    def test_smb_censoring(self):
+        example_handles = [
+            SMBHandle(
+                    SMBSource(
+                            "//SERVER/Resource", "username"),
+                    "~ocument.docx"),
+            SMBCHandle(
+                    SMBCSource(
+                            "//SERVER/Resource",
+                            "username", "topsecret", "WORKGROUP8"),
+                    "~ocument.docx"),
+        ]
+
+        for handle in example_handles:
+            with self.subTest(handle):
+                handle = handle.censor()
+
+                self.assertIsNone(handle.source._domain)
+                self.assertIsNone(handle.source._password)
+                self.assertIsNone(handle.source._user)
+
+    def test_nested_censoring(self):
+        handle = ZipHandle(
+                ZipSource(
+                        SMBCHandle(
+                                SMBCSource(
+                                        "//SERVER/Resource",
+                                        "username", driveletter="W"),
+                                "Confidential Documents.zip")),
+                "doc/Personal Information.docx")
+
+        self.assertIsNotNone(handle.source.handle.source._user)
+
+        handle = handle.censor()
+
+        self.assertIsNone(handle.source.handle.source._user)
