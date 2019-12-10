01c76f6811dfdedcb813fba36059803217ac8557
Alexander Faithfull <af@magenta.dk>
2019-12-10 13:48:34 +0100

    Added a pair of utility classes for handling multiple return values
    The new MultipleResources class is an unusual dict: its values all carry a
    reference to the object that contains them. This will eventually allow
    Resource and its subclasses to return a single focused value which also
    gives access to all other values computed at the same time

--- src/os2datascanner/engine2/model/utilities.py
+++ src/os2datascanner/engine2/model/utilities.py
@@ -1,5 +1,6 @@
 from os import rmdir, remove
 import os.path
+from weakref import proxy, ProxyTypes
 from tempfile import mkdtemp
 from contextlib import suppress
 
@@ -26,3 +27,84 @@ class NamedTemporaryResource:
             with suppress(FileNotFoundError):
                 rmdir(self._dir)
             self._dir = None
+
+
+class MultipleResults(dict):
+    """A MultipleResults is a subclass of dict that guarantees that all of the
+    values it produces -- including default values -- are instances of the
+    SingleResult class, and thus that they provide a (weak) reference back to
+    the MultipleResults that contains them."""
+
+    # Most of dict's methods are documented with a breezy "this just does
+    # D[k]=F[k]", but this is only true if you *don't* override __setitem__,
+    # so we need to do rather more work here than I might have liked...
+
+    def __init__(self, what=None, **kwargs):
+        super().__init__()
+        self.update(what, **kwargs)
+
+    def __setitem__(self, key, value):
+        return super().__setitem__(key, self._wrap(key, value))
+
+    def get(self, k, d=None):
+        return super().get(k, self._wrap(k, d))
+
+    def setdefault(self, k, d=None):
+        return super().setdefault(k, self._wrap(k, d))
+
+    def update(self, E=None, **F):
+        if E:
+            if hasattr(E, "keys"):
+                for k in E:
+                    self[k] = E[k]
+            else:
+                for k, v in E:
+                    self[k] = v
+        for k in F:
+            self[k] = F[k]
+
+    def _wrap(self, k, v):
+        if v is None:
+            return None
+        elif isinstance(v, SingleResult):
+            return SingleResult(self, k, v.value)
+        else:
+            return SingleResult(self, k, v)
+
+
+class SingleResult:
+    """A SingleResult is a single value stored in a MultipleResults."""
+    def __init__(self, parent, key, value):
+        self._parent = None
+        if parent is not None:
+            if isinstance(parent, ProxyTypes):
+                # Don't wrap proxies in proxies; weak references are confusing
+                # enough as is!
+                self._parent = parent
+            else:
+                self._parent = proxy(parent)
+        self._key = key
+        self._value = value
+
+    @property
+    def parent(self):
+        """Returns a weakref.CallableProxyType for the parent
+        MultipleResults, or None if none was specified."""
+        return self._parent
+
+    @property
+    def key(self):
+        """Returns the key associated with this SingleResult in the parent
+        MultipleResults."""
+        return self._key
+
+    @property
+    def value(self):
+        """Returns the value associated with this SingleResult."""
+        return self._value
+
+    def map(self, f):
+        """Returns a copy of this SingleResult, but with the value transformed
+        by the given function. (The parent MultipleResults will *not* be
+        updated.)"""
+        return SingleResult(self.parent, self.key, f(self.value))
