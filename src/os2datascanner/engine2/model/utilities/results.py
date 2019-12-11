11291e95c2464ca32dd7bda340afe891580deb57
Alexander Faithfull <af@magenta.dk>
2019-12-11 11:52:29 +0100

    Split the os2datascanner.engine2.model.utilities module up
    That name will still work, but now it's implemented by a few separate files
    in the new utilities/ folder

--- src/os2datascanner/engine2/model/utilities/results.py
+++ src/os2datascanner/engine2/model/utilities/results.py
@@ -1,33 +1,4 @@
-from os import rmdir, remove
-import os.path
 from weakref import proxy, ProxyTypes
-from tempfile import mkdtemp
-from contextlib import suppress
-
-class NamedTemporaryResource:
-    def __init__(self, name):
-        self._name = name
-        self._dir = None
-
-    def open(self, mode):
-        return open(self.get_path(), mode)
-
-    def get_path(self) -> str:
-        if self._dir is None:
-            self._dir = mkdtemp()
-        return os.path.join(self._dir, self._name)
-
-    def __enter__(self):
-        return self
-
-    def __exit__(self, exc_type, exc_value, backtrace):
-        if self._dir:
-            with suppress(FileNotFoundError):
-                remove(self.get_path())
-            with suppress(FileNotFoundError):
-                rmdir(self._dir)
-            self._dir = None
-
 
 class MultipleResults(dict):
     """A MultipleResults is a subclass of dict that guarantees that all of the
