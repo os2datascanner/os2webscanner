b8128c3b3de4fcebfd08424904bb2e48b2a8c83f
Alexander Faithfull <af@magenta.dk>
2020-01-06 16:03:55 +0100

    Added a test for engine2's tesseract wrapper
    This test fails at present, because there are many image formats tesseract
    doesn't handle properly. We can fix that by introducing an extra
    conversion phase to a well-supported format; for now, though, we just want
    to expose the problem

---
+++ src/os2datascanner/tests/test_engine2_ocr.py
@@ -0,0 +1,23 @@
+import os.path
+import unittest
+
+from os2datascanner.engine2.model.core import SourceManager
+from os2datascanner.engine2.model.file import FilesystemSource
+from os2datascanner.engine2.rules._transitional_conversions import (
+        image_processor)
+
+
+here_path = os.path.dirname(__file__)
+test_data_path = os.path.join(here_path, "data", "ocr")
+expected_result = "131016-9996"
+
+
+class TestEngine2OCR(unittest.TestCase):
+    def test_ocr_conversions(self):
+        fs = FilesystemSource(test_data_path)
+        with SourceManager() as sm:
+            for h in fs.handles(sm):
+                self.assertEqual(
+                        image_processor(h.follow(sm)),
+                        expected_result,
+                        "{0} failed".format(h))
