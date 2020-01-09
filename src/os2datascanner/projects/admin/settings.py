62d7afe196351fd5813056e43da400fb1b8094ea
Alexander Faithfull <af@magenta.dk>
2020-01-09 15:09:22 +0100

    Made the administration interface default to using engine2 scans

--- src/os2datascanner/projects/admin/settings.py
+++ src/os2datascanner/projects/admin/settings.py
@@ -211,7 +211,7 @@ DO_USE_GROUPS = False
 DO_USE_MD5 = True
 
 # Use engine2 components in the scan process where possible?
-USE_ENGINE2 = False
+USE_ENGINE2 = True
 
 # The threshold for number of OCR conversion queue items per scan above which
 # non-OCR conversion will be paused. The reason to have this feature is that
