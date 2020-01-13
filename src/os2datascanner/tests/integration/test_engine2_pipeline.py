cb99f92df5b13af3e182e5e069d57e52ea567375
Alexander Faithfull <af@magenta.dk>
2020-01-13 11:13:38 +0100

    Host all of the pipeline stages in the integration test process
    Ah, stateless code is so neat! This should mean that we get meaningful
    coverage results for the pipeline

--- src/os2datascanner/tests/integration/test_engine2_pipeline.py
+++ src/os2datascanner/tests/integration/test_engine2_pipeline.py
@@ -4,8 +4,10 @@ from    json import dumps, loads
 import  pika
 import  base64
 import  unittest
-import  subprocess
 
+from    os2datascanner.engine2.pipeline import (
+        explorer, processor, matcher, tagger, exporter)
+from    os2datascanner.engine2.pipeline.utilities import json_event_processor
 from    os2datascanner.engine2.model.core import Source
 from    os2datascanner.engine2.rules.regex import RegexRule
 from    os2datascanner.engine2.rules.logical import OrRule
@@ -47,61 +49,49 @@ expected_matches = [
 ]
 
 
-def python(*args):
-    """Starts a new instance of this Python interpreter with the specified
-    arguments. Standard output and standard error will be passed through."""
-    return subprocess.Popen([sys.executable, *args])
-
-
 class StopHandling(Exception):
     pass
 
 
+@json_event_processor
+def message_received(body, channel):
+    if channel == "os2ds_scan_specs":
+        return explorer.message_received_raw(body, channel,
+                "os2ds_conversions", "os2ds_problems")
+    elif channel == "os2ds_conversions":
+        return processor.message_received_raw(body, channel,
+                "os2ds_representations", "os2ds_scan_specs")
+    elif channel == "os2ds_representations":
+        return matcher.message_received_raw(body, channel,
+                "os2ds_matches", "os2ds_handles", "os2ds_conversions")
+    elif channel == "os2ds_handles":
+        return tagger.message_received_raw(body, channel,
+                "os2ds_metadata")
+    elif channel in ("os2ds_matches", "os2ds_metadata", "os2ds_problems",):
+        return exporter.message_received_raw(body, channel,
+                False, "os2ds_results")
+
+
 class Engine2PipelineTests(unittest.TestCase):
     def setUp(self):
-        amqp_host = getenv("AMQP_HOST", "localhost")
-
-        python("-m", "os2datascanner.engine2.pipeline._consume_queue",
-                "--host", amqp_host,
-                "os2ds_scan_specs", "os2ds_conversions",
-                "os2ds_representations", "os2ds_matches", "os2ds_handles",
-                "os2ds_metadata", "os2ds_problems", "os2ds_results").wait()
-        self.explorer = python(
-                "-m", "os2datascanner.engine2.pipeline.explorer",
-                "--host", amqp_host)
-        self.processor = python(
-                "-m", "os2datascanner.engine2.pipeline.processor",
-                "--host", amqp_host)
-        self.matcher = python(
-                "-m", "os2datascanner.engine2.pipeline.matcher",
-                "--host", amqp_host)
-        self.tagger = python(
-                "-m", "os2datascanner.engine2.pipeline.tagger",
-                "--host", amqp_host)
-        self.exporter = python(
-                "-m", "os2datascanner.engine2.pipeline.exporter",
-                "--host", amqp_host)
-
         parameters = pika.ConnectionParameters(
-                host=amqp_host,
+                host=getenv("AMQP_HOST", "localhost"),
                 heartbeat=6000)
         self.connection = pika.BlockingConnection(parameters)
         self.channel = self.connection.channel()
 
-        self.channel.queue_declare("os2ds_scan_specs",
-                passive=False, durable=True,
-                exclusive=False, auto_delete=False)
-        self.channel.queue_declare("os2ds_results",
-                passive=False, durable=True,
-                exclusive=False, auto_delete=False)
+        for c in ("os2ds_scan_specs", "os2ds_conversions",
+                "os2ds_representations", "os2ds_matches", "os2ds_handles",
+                "os2ds_metadata", "os2ds_results", "os2ds_problems"):
+            self.channel.queue_declare(c,
+                    passive=False, durable=True,
+                    exclusive=False, auto_delete=False)
+            if c != "os2ds_results":
+                self.channel.basic_consume(c, message_received)
 
     def tearDown(self):
         self.channel.stop_consuming()
         self.connection.close()
-        for p in (self.explorer, self.processor, self.matcher, self.tagger,
-                self.exporter):
-            p.kill()
-            p.wait()
 
     def test_simple_regex_match(self):
         print(Source.from_url(data_url).to_json_object())
@@ -116,13 +106,13 @@ class Engine2PipelineTests(unittest.TestCase):
                 body=dumps(obj).encode())
 
         messages = {}
-        def message_received(a, b, c, d):
+        def result_received(a, b, c, d):
             body = loads(d.decode("utf-8"))
             messages[body["origin"]] = body
             if len(messages) == 2:
                 raise StopHandling()
 
-        self.channel.basic_consume("os2ds_results", message_received)
+        self.channel.basic_consume("os2ds_results", result_received)
 
         try:
             self.channel.start_consuming()
