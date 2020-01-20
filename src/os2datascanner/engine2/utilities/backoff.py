9848fa9e6abb0c9cb97abfd7f38b3713a15b26a5
Alexander Faithfull <af@magenta.dk>
2020-01-20 13:55:15 +0100

    Added a utility function for retrying operations with exponential backoff
    The Exchange Web Services API seems to have some quite strict rules for how
    often one can interact with the server; this function should let us deal
    with that and with similar services

---
+++ src/os2datascanner/engine2/utilities/backoff.py
@@ -0,0 +1,33 @@
+from time import sleep
+
+
+def run_with_backoff(
+        op, *exception_set,
+        count=0, max_tries=10, ceiling=6, base=1):
+    """Performs an operation until it succeeds (or until the maximum number of
+    attempts is hit), with exponential backoff after each failure. On success,
+    returns a (result, parameters) pair; expanding the parameters dictionary
+    and giving it to this function will allow the backoff behaviour to be
+    persistent."""
+    count = max(0, count)
+    base = max(0.01, base)
+    end = count + max_tries
+    while count < end:
+        if count:
+            max_delay = base * (2 ** (min(count, ceiling) - 1))
+            sleep(max_delay)
+        try:
+            return (op(), dict(
+                    count=count, max_tries=max_tries, ceiling=ceiling,
+                    base=base))
+        except Exception as e:
+            if isinstance(e, exception_set):
+                # This exception indicates that we should back off and try
+                # again
+                count += 1
+                if count == max_tries:
+                    # ... but we've exhausted our maximum attempts
+                    raise
+            else:
+                # This is not a backoff exception -- raise it immediately
+                raise
