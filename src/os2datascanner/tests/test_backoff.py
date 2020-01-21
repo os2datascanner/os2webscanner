caab35674c8ae4a2bc9d07886cd09716af219492
Alexander Faithfull <af@magenta.dk>
2020-01-21 11:08:43 +0100

    Fixed a few bugs in the exponential backoff function tests
    The fail_if_too_busy function never actually updated its timestamp
    properly: it updated a local variable with the same name instead. This only
    affected the test_memory test, which would actually have failed if not for
    this bug, because the second part of the test forgot to actually use the
    state from the first part...

--- src/os2datascanner/tests/test_backoff.py
+++ src/os2datascanner/tests/test_backoff.py
@@ -18,7 +18,7 @@ class ImpatientClientFauxPas(ImpoliteClientFauxPas):
 
 class TestBackoff(unittest.TestCase):
     def setUp(self):
-        self.last_call_at = time()
+        self.reset_busy_time()
 
     def fail_if_too_busy(self):
         now = time()
@@ -28,7 +28,10 @@ class TestBackoff(unittest.TestCase):
             else:
                 return "Successful result"
         finally:
-            last_call_at = now
+            self.last_call_at = now
+
+    def reset_busy_time(self):
+        self.last_call_at = time()
 
     def test_base_failure(self):
         with self.assertRaises(ImpatientClientFauxPas):
@@ -61,21 +64,26 @@ class TestBackoff(unittest.TestCase):
     def test_memory(self):
         first_start = time()
         # This call should take ~15 (1+2+4+8) seconds, and we should succeed
-        # after first backing off three times
+        # after first backing off four times
         result, params = run_with_backoff(
                 self.fail_if_too_busy, ImpatientClientFauxPas)
         first_duration = time() - first_start
         self.assertEqual(
                 params["count"],
-                3)
+                4)
+
+        self.reset_busy_time()
 
         # Now that we have those results (i.e., now that we don't need to try
         # backoff durations of 1, 2, and 4 seconds), we should always be able
         # to finish more quickly (in ~8 seconds)
         second_start = time()
-        run_with_backoff(self.fail_if_too_busy, ImpatientClientFauxPas)
+        run_with_backoff(
+                self.fail_if_too_busy, ImpatientClientFauxPas, **params)
         second_duration = time() - second_start
 
+        # Strictly speaking we should be 7 seconds faster, but saying 6 gives
+        # us a 14% margin of error in case the CI system is having a tough day
         self.assertTrue(
-                second_duration < first_duration,
+                (second_duration + 6) < first_duration,
                 "learned backoff slower than naive backoff(?)")
