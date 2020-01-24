3c400739a97627acb8ea6b21ca0d09ee38a7b229
Alexander Faithfull <af@magenta.dk>
2020-01-24 16:27:02 +0100

    Introduced a random fuzz factor to the exponential backoff function
    If we have eight processors and they all wait eight seconds before hitting
    a paranoid remote server at the same time, then, er, well, there's not much
    point in waiting; we're going to get throttled anyway. Allowing for a bit
    of random drift might be a good solution to that problem

--- src/os2datascanner/engine2/utilities/backoff.py
+++ src/os2datascanner/engine2/utilities/backoff.py
@@ -1,26 +1,35 @@
 from sys import stderr
 from time import sleep
+from random import random
 
 
 def run_with_backoff(
         op, *exception_set,
-        count=0, max_tries=10, ceiling=7, base=1, warn_after=6):
+        count=0, max_tries=10, ceiling=7, base=1, warn_after=6, fuzz=0):
     """Performs an operation until it succeeds (or until the maximum number of
     attempts is hit), with exponential backoff after each failure. On success,
     returns a (result, parameters) pair; expanding the parameters dictionary
     and giving it to this function will allow the backoff behaviour to be
     persistent."""
-    count = max(0, count)
+    count = max(0, int(count))
     base = max(0.01, base)
     end = count + max_tries
+    fuzz = max(min(fuzz or 0, 1), 0)
     while count < end:
         if count:
             max_delay = base * (2 ** (min(count, ceiling) - 1))
+            if fuzz:
+                # Sleep time is randomly adjusted by the proportion given in
+                # the fuzz parameter (0 - 0% adjustment, 1 - ±100% adjustment)
+                fuzz_diff = (max_delay * fuzz)
+                adj = -fuzz_diff + (2 * random() * fuzz_diff)
+                print(fuzz, fuzz_diff, adj, max_delay)
+                max_delay += adj
             sleep(max_delay)
         try:
             return (op(), dict(
                     count=count, max_tries=max_tries, ceiling=ceiling,
-                    base=base))
+                    base=base, fuzz=fuzz))
         except Exception as e:
             if isinstance(e, exception_set):
                 # This exception indicates that we should back off and try
@@ -39,8 +48,9 @@ def run_with_backoff(
                     max_delay = base * (2 ** (min(count, ceiling) - 1))
                     print("warning: while executing {0}"
                             " with backoff: failed {1} times,"
-                            " delaying for {2} seconds".format(
-                                    str(op), count, max_delay),
+                            " delaying for {2}±{3:.1f} seconds".format(
+                                    str(op), count, max_delay,
+                                    max_delay * fuzz),
                             file=stderr)
             else:
                 # This is not a backoff exception -- raise it immediately
