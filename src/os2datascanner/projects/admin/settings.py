8d35b107905b2a691c04ed449e518fd5534ba9ea
Steffen Jørgensen <sj@magenta-aps.dk>
2020-01-24 10:42:02 +0100

    create components

--- src/os2datascanner/projects/admin/settings.py
+++ src/os2datascanner/projects/admin/settings.py
@@ -33,15 +33,14 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'APP_DIRS': True,
         'OPTIONS': {
-            'debug': DEBUG,
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'django_settings_export.settings_export',
-             ],
-         },
+            ],
+        },
     },
 ]
 
