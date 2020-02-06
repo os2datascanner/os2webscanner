f63e7380b29bf2536cb6cf6d92a1b244fdf1ae42
Steffen Jørgensen <sj@magenta-aps.dk>
2020-02-06 16:48:40 +0100

    Added docs for modal to Designguide

--- src/os2datascanner/projects/admin/adminapp/static/src/js/index.js
+++ src/os2datascanner/projects/admin/adminapp/static/src/js/index.js
@@ -1,3 +1,18 @@
 import '../css/master.scss';
 
 console.log('Datascanner sees all');
+
+$(document).ready(function(){
+  $("a[data-modal='modal:open']").click(function(e){
+      e.preventDefault()
+
+      // Find the target element (same as our href)
+      var target = $(this).attr("href")
+
+      // Find the src to set
+      var src = $(this).attr("data-src")
+
+      // Find the iframe in our target and set its src
+      $(target).find("iframe").attr("src",src);
+  })
+})
