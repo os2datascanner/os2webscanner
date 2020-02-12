5760c7b90aa2b6b1745eab69ef1dd68d8fc5aab7
Alexander Faithfull <af@magenta.dk>
2020-02-12 14:29:56 +0100

    Added a new rule for checking that a conversion exists
    We're getting a bit meta here (thus the module name!), but the idea is
    that it should be possible to explicitly gate some rules: it doesn't make
    any sense to check the image dimensions of an arbitrary object

--- src/os2datascanner/engine2/rules/__init__.py
+++ src/os2datascanner/engine2/rules/__init__.py
@@ -1,6 +1,7 @@
-from . import cpr # noqa
-from . import dummy # noqa
-from . import regex # noqa
-from . import logical # noqa
-from . import dimensions # noqa
-from . import last_modified # noqa
+from . import cpr  # noqa
+from . import meta  # noqa
+from . import dummy  # noqa
+from . import regex  # noqa
+from . import logical  # noqa
+from . import dimensions  # noqa
+from . import last_modified  # noqa
