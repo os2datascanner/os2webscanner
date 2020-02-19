42fa7b5704966424c8722ec2cb5acb28efb36be5
Alexander Faithfull <af@magenta.dk>
2020-02-19 14:41:44 +0100

    Added Remediator and DefaultRole to the Django admin pages
    Ideally we'd have a single UI for all Role subclasses -- having to manage
    roles in this isolated manner is not very helpful. But it's a start

--- src/os2datascanner/projects/report/reportapp/admin.py
+++ src/os2datascanner/projects/report/reportapp/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
+from .models.roles.remediator_model import Remediator
+from .models.roles.defaultrole_model import DefaultRole
 from .models.aliases.adsidalias_model import ADSIDAlias
 from .models.aliases.emailalias_model import EmailAlias
 from .models.documentreport_model import DocumentReport
@@ -15,3 +17,11 @@ class ADSIDAliasAdmin(admin.ModelAdmin):
 @admin.register(EmailAlias)
 class EmailAliasAdmin(admin.ModelAdmin):
     list_display = ('address', 'user', )
+
+@admin.register(DefaultRole)
+class DefaultRoleAdmin(admin.ModelAdmin):
+    list_display = ('user', )
+
+@admin.register(Remediator)
+class RemediatorAdmin(admin.ModelAdmin):
+    list_display = ('user', )
