807f884b34e3010efbfaaad96ca82f4b3fce311f
Novik <ns@magenta.dk>
2020-02-26 09:25:14 +0100

    add active menu links

---
+++ src/os2datascanner/projects/admin/adminapp/static/js/activeMenu.js
@@ -0,0 +1,11 @@
+//Active menu links
+function setActive() {
+  aObj = document.getElementById('navigation').getElementsByTagName('a');
+  for(i=0;i<aObj.length;i++) { 
+    if(document.location.href.indexOf(aObj[i].href)>=0) {
+      aObj[i].className='active';
+    }
+  }
+}
+
+window.onload = setActive;
