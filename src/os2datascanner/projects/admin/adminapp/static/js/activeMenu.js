c0c10323670eaeba24a87db60a0c25a6bff8844b
Novik <ns@magenta.dk>
2020-03-04 11:44:21 +0100

    update documentation

--- src/os2datascanner/projects/admin/adminapp/static/js/activeMenu.js
+++ src/os2datascanner/projects/admin/adminapp/static/js/activeMenu.js
@@ -1,8 +1,14 @@
-// Set active link in menu
+/**
+ * Set active link in the topbar menu
+ * 
+ * @param {object} aObj   All a tag links in the topbar menu.
+ */
 function setActive() {
   aObj = document.getElementById('navigation').getElementsByTagName('a');
-  for(i=0;i<aObj.length;i++) { 
+  for(i=0;i<aObj.length;i++) {
+    // Loop over all a tags.
     if(document.location.href.indexOf(aObj[i].href)>=0) {
+      // if the a tag is clicked add active to the link.
       aObj[i].className='active';
     }
   }
